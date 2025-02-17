fac944e7afbe29dd5ab4e228b1d53dec98a69714
elanetto <101054274+elanetto@users.noreply.github.com>
2025-02-17 20:48:33 +0100

    Commented out local SQL host and added API

--- public/js/api/apiConfig.js
+++ public/js/api/apiConfig.js
@@ -1,5 +1,5 @@
-// const API_BASE_URL = "https://frontend-notes-ecn4.onrender.com";
-const API_BASE_URL = "http://localhost:3000";
+const API_BASE_URL = "https://frontend-notes-ecn4.onrender.com";
+// const API_BASE_URL = "http://localhost:3000";
 
 export const API_ENDPOINTS = {
   LOGIN: `${API_BASE_URL}/login`,
